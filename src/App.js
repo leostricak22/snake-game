993efa94e40a8e67896d8839eecb3c0486b5183b
leostricak22 <stricak.leo@gmail.com>
2024-09-17 20:29:12 +0200

    feat: Add result and welcome modal with name input and leaderboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Gameboard from "./components/Gameboard";
 import sign from "./assets/sign.png";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
+import MainMenu from "./components/MainMenu";
+
+const INITIAL_PLAYER_POSITION = {
+    positions: [{ x: 5, y: 5 }],
+    direction: 'r'
+}
 
 function App() {
-    const [playerPosition, setPlayerPosition] = useState({
-        positions: [{ x: 5, y: 5 }],
-        direction: 'r'
-    });
+    const [playerPosition, setPlayerPosition] = useState(INITIAL_PLAYER_POSITION);
 
-    const [gameState, setGameState] = useState(true);
+    const [gameState, setGameState] = useState(false);
+    const [playerName, setPlayerName] = useState("")
+    const dialog = useRef();
 
     function handleKeyDown(event) {
         setPlayerPosition(prevPlayerPosition => {
@@ -65,8 +70,24 @@ function App() {
         };
     }, []);
 
+    useEffect(() => {
+        if(!gameState)
+            dialog.current.open();
+    }, [gameState]);
+
+    function startGame() {
+        if(!playerName) {
+            alert("Upišite ime.")
+            return false
+        }
+
+        setPlayerPosition(INITIAL_PLAYER_POSITION)
+        setGameState(true)
+    }
+
     return (
         <div id="main" tabIndex="0" onKeyDown={handleKeyDown}>
+            {!gameState && <MainMenu ref={dialog} startGame={startGame} setPlayerName={setPlayerName} playerName={playerName} score={playerPosition.positions.length-1}/>}
             <Gameboard
                 playerPosition={playerPosition}
                 setPlayerPosition={setPlayerPosition}
@@ -74,7 +95,7 @@ function App() {
                 setGameState={setGameState}
             />
             <div className="signContainer">
-                <h2 className="top-left">{playerPosition.positions.length-2}</h2>
+                <h2 className="top-left">{playerPosition.positions.length-1}</h2>
                 <img src={sign} alt={"sign"} className="signImage"/>
             </div>
         </div>
---
+++ src/components/MainMenu.js
@@ -0,0 +1,62 @@
+import {forwardRef, useImperativeHandle, useRef} from "react";
+import { createPortal } from 'react-dom'
+
+const MainMenu = forwardRef(function MainMenu({startGame, playerName, setPlayerName, score}, ref) {
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => ({
+        open() {
+            dialog.current.showModal();
+        }
+    }));
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Escape") {
+            e.preventDefault();
+        }
+    };
+
+    const handleNameChange = (e) => {
+        setPlayerName(e.target.value);
+    };
+
+    return createPortal (
+        (
+            <>
+                <div className="modal-overlay"></div>
+                <dialog className="result-modal" ref={dialog} onKeyDown={handleKeyDown}>
+                    <div className="modal-container">
+                        <div className="modal-content">
+                            <h2>Zmijica</h2>
+                            <h3>Osvojili ste: <strong>{score}</strong> bodova</h3>
+                            <p>Upišite ime da bi mogli započeti igru:</p>
+                            <input
+                                type="text"
+                                placeholder="Upišite ime"
+                                className="name-input"
+                                required
+                                value={playerName}
+                                onChange={handleNameChange}
+                            />
+                            <button className="start-btn" onClick={startGame}>Započni</button>
+                        </div>
+
+                        <div className="leaderboard-container">
+                            <h3>Ljestvica</h3>
+                            <ul className="leaderboard-list">
+                                <li>1. Player1 - 100</li>
+                                <li>2. Player2 - 95</li>
+                                <li>3. Player3 - 90</li>
+                                <li>4. Player4 - 85</li>
+                                <li>5. Player5 - 80</li>
+                            </ul>
+                        </div>
+                    </div>
+            </dialog>
+        </>
+    ),
+    document.getElementById("modal")
+    )
+})
+
+export default MainMenu;
